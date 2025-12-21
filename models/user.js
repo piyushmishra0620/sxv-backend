3b1b3030c1fed83e1a03a055077c7c10aaff2631
Satyajit-tech2006 <satyajitswain2006@gmail.com>
2025-12-21 10:34:23 +0530

    feat(user): enhance user schema with additional fields and validations
    - Added unique constraint to email field.
    - Introduced phone, branch, and graduationYear fields as required.
    - Set default values for regdNo, college, paymentStatus, paymentType, and ticketGenerated fields.
    - Removed redundant fields and organized schema structure for clarity.

--- models/user.js
+++ models/user.js
@@ -1,40 +1,77 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isVssutian: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    regdNo: {
-        type: String,
-    },
-    events: [
-        {
-            type: mongoose.Types.ObjectId, ref: "Event"
-        }
-    ],
-    college: { type: String, required: true },
-    graduationYear: { type: Number, required: true },
-    branch: { type: String, required: true },
-    paymentStatus: { type: Boolean, required: true, default: false },
-    paymentType: {type: Number},
-    ticketGenerated: { type: Boolean, required: true, default: false },
-    phone: { type: String, required: true }
-}, { timestamps: true })
 
-module.exports = mongoose.model('User', userSchema)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    branch: {
+      type: String,
+      required: true,
+    },
+
+    graduationYear: {
+      type: Number,
+      required: true,
+    },
+
+    isVssutian: {
+      type: Boolean,
+      default: false,
+    },
+
+    regdNo: {
+      type: String,
+      default: null,
+    },
+
+    college: {
+      type: String,
+      default: "Non-VSSUT",
+    },
+
+    events: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Event",
+      },
+    ],
+
+    paymentStatus: {
+      type: Boolean,
+      default: false,
+    },
+
+    paymentType: {
+      type: Number,
+      default: 0,
+    },
+
+    ticketGenerated: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
